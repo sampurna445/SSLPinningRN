0646f574a9977efa833dcc9cc56414d2f0eaed27
sampurna445 <128822770+sampurna445@users.noreply.github.com>
2023-11-02 16:42:35 +0000

    SSL Pinning Iplmented in andrroid

---
+++ App.js
@@ -0,0 +1,36 @@
+import {View, Text, SafeAreaView, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {fetch} from 'react-native-ssl-pinning';
+
+export default function App() {
+  const secureFetchData = async () => {
+    console.log('fetch data clicked');
+    try {
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos/1',
+        {
+          method: 'GET',
+          sslPinning: {
+            certs: ['mycert'],
+          },
+        },
+      );
+
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log('Data:', data);
+      } else {
+        console.log('Request failed with status:', response.status);
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+      <Button title="Fetch Secure Data" onPress={secureFetchData} />
+    </SafeAreaView>
+  );
+}
